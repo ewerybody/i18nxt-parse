812f35328f1f8ef108cb0ff47c2a703b3e89a909
Eric Werner <ewerybody+github@gmail.com>
2026-05-10 00:59:12 +0200

    Update i18nxt_parse.py
    implemented plural parsing #3
    extracted `_get_pattern_list` for a bit more DRYness
    new parsing structure into keys[context[plurals, value]] for future enhancements.

--- i18nxt_parse.py
+++ i18nxt_parse.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 
 from moz.l10n.model import (
+    CatchallKey,
     Comment,
     Entry,
     Expression,
@@ -16,9 +17,14 @@ from moz.l10n.model import (
     VariableRef,
 )
 
-
-PLURALS = 'zero', 'one', 'two', 'few', 'many', 'other'
+OTHER = 'other'
+PLURALS = 'zero', 'one', 'two', 'few', 'many', OTHER
 RE_INTERPOLATION = re.compile(r'\{\{(\w+)\}\}')
+
+# plural interpolation has to happen with {{count}}! See:
+# https://www.i18next.com/translation-function/plurals
+COUNT = 'count'
+COUNT_DECLARATION = {COUNT: Expression(VariableRef(COUNT), 'number')}
 
 
 def i18next_parse(source: str | bytes | Path) -> Resource:
@@ -34,38 +40,83 @@ def i18next_parse(source: str | bytes | Path) -> Resource:
         if source_path.is_file():
             source = source_path.read_text()
 
-    data = json.loads(source)
+    # first pass, gather keys split by _ into key, context, plural. No _ no context ('')!
+    keys: dict[str, dict[str, dict[str, str]]] = {}
+    """Dictionary of keys[context[plural, value]]"""
+    for key, value in json.loads(source).items():
+        if isinstance(value, (list, dict)):
+            raise NotImplementedError('So far there is no support for arrays and nesting!')
 
-    entries: list[Entry[Message] | Comment] = []
-    for key, value in data.items():
-        plural_keys = {}
+        if not isinstance(value, str):
+            raise TypeError(f'Wrong value type! No support for {type(value)}')
 
         if '_' not in key:
-            if '{{' not in value:
+            keys.setdefault(key, {}).setdefault('', {})[''] = value
+            continue
+
+        key, *parts = key.split('_')
+        if len(parts) > 2:
+            raise ValueError(
+                'Unsupported amount of parts in key! '
+                'Can be either "key", "key_contexy/plural" or "key_context_plural"'
+            )
+
+        # plural suffixes always come last. See:
+        # https://www.i18next.com/translation-function/context#combining-with-plurals
+        if parts[-1] in PLURALS:
+            plural = parts[-1]
+            context = parts[0] if len(parts) == 2 else ''
+        else:
+            plural = ''
+            context = parts[0] if len(parts) == 1 else ''
+        keys.setdefault(key, {}).setdefault(context, {})[plural] = value
+
+    entries: list[Entry[Message] | Comment] = []
+    for key, contexts in keys.items():
+        for context, plurals in contexts.items():
+            if context != '':
+                raise NotImplementedError('No support yet for other than default contexts ("")!')
+
+            if len(plurals) == 1 and '' in plurals:
                 entries.append(
-                    Entry(
-                        id=(key,),
-                        value=PatternMessage([value]),
-                    )
+                    Entry(id=(key,), value=PatternMessage(_get_pattern_list(plurals[''])))
                 )
                 continue
 
-            pattern: list[str | Expression | Markup] = []
-            for i, part in enumerate(RE_INTERPOLATION.split(value)):
-                if not part:
-                    continue
-                pattern.append(Expression(VariableRef(part)) if i % 2 else part)
+            # Apparently bare keys could act as 'one' if other plurals exist:
+            # https://www.i18next.com/translation-function/nesting#passing-options-to-nestings
+            if '' in plurals:
+                if 'one' in plurals:
+                    raise NotImplementedError('Bare key cannot be "one" if that already exists!')
+                plurals['one'] = plurals['']
+                del plurals['']
 
-            entries.append(
-                Entry(
-                    id=(key,),
-                    value=PatternMessage(pattern),
-                )
+            variants: dict = {
+                (p,) if p != 'other' else (CatchallKey('other'),): _get_pattern_list(plurals[p])
+                for p in PLURALS
+                if p in plurals
+            }
+            msg = SelectMessage(
+                declarations=COUNT_DECLARATION,
+                selectors=(VariableRef(COUNT),),
+                variants=variants,
             )
-        else:
-            parts = key.split('_')
+            entries.append(Entry(id=(key,), value=msg))
 
     return Resource(Format.plain_json, [Section((), entries)])
+
+
+def _get_pattern_list(value: str) -> list[str | Expression | Markup]:
+    if '{{' not in value:
+        return [value]
+
+    pattern: list[str | Expression | Markup] = []
+    for i, part in enumerate(RE_INTERPOLATION.split(value)):
+        if not part:
+            continue
+        # regex split puts our {{variables}} in the even spots all other in odds
+        pattern.append(Expression(VariableRef(part)) if i % 2 else part)
+    return pattern
 
 
 if __name__ == '__main__':
